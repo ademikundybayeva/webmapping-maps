c9e42e89f509f6d44fd330ebb734994eb4c5a7a5
ademikundybayeva <98976113+ademikundybayeva@users.noreply.github.com>
2022-04-20 11:32:48 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -13,8 +13,7 @@ async function addDistrictsGeoJson(url) {
       polygons.addTo(map)}
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
-function popUPinfo(feature, layer) {
-      layer.bindPopup(feature.properties.NIMI)}
+function popUPinfo(feature, layer) {layer.bindPopup(feature.properties.NIMI)}
 
 async function addDistrictsGeoJson(url) {
       const response = await fetch(url)
