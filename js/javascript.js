5b2d105d5ce24b9d362f8fa71b67ef7205c46150
ademikundybayeva <98976113+ademikundybayeva@users.noreply.github.com>
2022-04-25 10:35:07 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -66,7 +66,9 @@ async function addCelltowersGeoJson(url) {
       const response = await fetch(url)
       const data = await response.json()
       const markers = L.geoJson(data)
-      markers.addTo(map)}
+      const clusters = L.markerClusterGroup()
+      clusters.addLayer(markers)
+      clusters.addTo(map) }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 function createCircle(feature, latlng) {
