259a36c49e893250609cad54e84cf63aa29998b7
ademikundybayeva <98976113+ademikundybayeva@users.noreply.github.com>
2022-04-27 10:41:55 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,30 +1,18 @@
-addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: 'OpenStreetMap contributors',
+      })
+osm.addTo(map)
 
+addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 async function addGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
   console.log(data.features[0])
 }
-{
-  "type": "Feature",
-    "properties": {
-      "radio": "UMTS",
-        "mcc": 248,
-          "net": 1,
-            "area": 7,
-              "cell": 196901,
-                "unit": 0,
-                  "lon": 26.726892,
-                    "lat": 58.381053,
-                      "range": 1000,
-                        "samples": 10,
-                          "changeable": 1,
-                            "created": 1230291978,
-                              "updated": 1248350450,
-                                "averageSig": 0
-    },
-      "geometry": {
-        "type": "Point",
-          "coordinates": [26.726892, 58.381053]
-      }
+
+function defaultMapSettings() {
+      map.setView([58.373523, 26.716045], 12)
 }
